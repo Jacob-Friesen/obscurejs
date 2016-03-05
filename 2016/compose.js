52019c7152f038677e792505376022af29266275
Jacob Friesen <jacob@jacobfriesen.com>
2016-03-04 20:25:40 -0500

    (feat) Code for the compose post

---
+++ 2016/compose.js
@@ -0,0 +1,69 @@
+#!/bin/sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+'use strict';
+
+// The original call
+console.log('Before the prepended api call:');
+
+// Simulate an API call.
+var apiCall = function(callback) {
+  setTimeout(function() {
+    callback(['some', 'results']);
+  }, 500);
+};
+
+var success = function(results) {
+  console.log('received results:', results);
+};
+
+var wrappedApiCall = function() {
+  apiCall(success);
+};
+wrappedApiCall();
+// received results: [ 'some', 'results' ]
+
+// The call prepended with a function
+var preSuccessMessage = function() {
+  console.log('Calling before success');
+};
+
+setTimeout(function() {
+  console.log('\nAdded a prepended function:');
+
+  wrappedApiCall = function() {
+    apiCall(function() {
+      preSuccessMessage();
+      success.apply(this, arguments);
+      // Calling before success
+      // received results: [ 'some', 'results' ]
+    });
+  };
+  wrappedApiCall();
+}, 600);
+
+
+// The call prepended with a function using a preCall
+
+var compose = function() {
+  var callbacks = Array.prototype.slice.call(arguments);
+
+  return function() {
+    var args = arguments,
+        self = this;
+
+    callbacks.forEach(function(callback) {
+      callback.apply(self, args);
+    });
+  };
+};
+
+setTimeout(function() {
+  console.log('\nAdded a prepended function with compose:');
+
+  wrappedApiCall = function() {
+    apiCall(compose(preSuccessMessage, success));
+    // Calling before success
+    // received results: [ 'some', 'results' ]
+  };
+  wrappedApiCall();
+}, 1200);
--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Each example corresponds directly to a blog post:
 
 <h5>2016</h5>
 <hr/>
-* [functionToString.js](#): About the Function.toString method and how it can be used for debugging and function regeneration.
+* [compose.js](#): A function to store functions to be called later.
+* [functionToString.js](https://github.com/Jacob-Friesen/obscurejs/blob/master/2016/functionToString.js): About the Function.toString method and how it can be used for debugging and function regeneration.
 * [observable.js](http://obscurejavascript.tumblr.com/post/137500537300/observables-in-javascript): How to treat data that arrives over time in an array like fashion.
 * [proxy.js](http://obscurejavascript.tumblr.com/post/138429574082/proxies-in-javascript): How to change JavaScript syntax on given objects via the Proxy API.
 * [replace3rdArg.js](http://obscurejavascript.tumblr.com/post/139312155038/the-stringreplace-callback-in-javascript): How to use the little known ability of String.replace to accept callbacks.

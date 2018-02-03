3ef6b14befa112c42576574a067cf1d28ba0e02d
Jacob Friesen <jacob@jacobfriesen.com>
2018-02-02 20:57:07 -0500

    (feat) Added indexOf extra case for example

--- 2018/customArrayOrder.js
+++ 2018/customArrayOrder.js
@@ -16,12 +16,12 @@ function getListForTable(callback) {
     // Make it ordered like the UI is. In a real application this information
     // could then be displayed in a permission table.
     const uiOrder = ['dashboard', 'reports', 'admin'];
-    //     const orderMap = {};
-    //     uiOrder.forEach((name, index) => orderMap[name] = index);
+    const orderMap = {};
+    uiOrder.forEach((name, index) => orderMap[name] = index);
 
     const orderedPolicies = [];
     policies.forEach((policy) => {
-      const position = uiOrder.indexOf(policy.name); // skip orderMap
+      const position = orderMap[policy.name];
       orderedPolicies[position] = policy;
     });
 
@@ -30,9 +30,38 @@ function getListForTable(callback) {
 }
 
 getListForTable((orderedPolicies) => {
-  console.log('Ordered for the UI:');
+  console.log('\nOrdered for the UI (Optimized):');
   console.log(orderedPolicies);
   // [ { name: 'dashboard', readonly: true, editable: true },
   //   { name: 'reports', readonly: true, editable: false },
   //   { name: 'admin', readonly: false, editable: false } ]
+  
+  part2();
 });
+
+function getListForTable2(callback) {
+  apiCall((policies) => {
+    // Make it ordered like the UI is. In a real application this information
+    // could then be displayed in a permission table.
+    const uiOrder = ['dashboard', 'reports', 'admin'];
+    const orderedPolicies = [];
+
+    policies.forEach((policy) => {
+      // This means checking through a whole array, so only use when uiOrder is small
+      const position = uiOrder.indexOf(policy.name);
+      orderedPolicies[position] = policy;
+    });
+
+    callback(orderedPolicies)
+  });
+}
+
+function part2() {
+  getListForTable2((orderedPolicies) => {
+    console.log('\nOrdered for the UI (With indexOf):');
+    console.log(orderedPolicies);
+    // [ { name: 'dashboard', readonly: true, editable: true },
+    //   { name: 'reports', readonly: true, editable: false },
+    //   { name: 'admin', readonly: false, editable: false } ]
+  });
+}
